bdc540cf6b4aa046a12ce613894a7f564bbe8437
Carl de Billy <carl.debilly@nventive.com>
2016-04-18 20:07:32 +0000

    [UniversalImageLoader-Android] Patched a concurrency issue "per uri" for a loading asset. #noauto ***NO_CI***
    work-items: #19713, #19906, #20205
    git-tfs-id: [http://tfs04:8080/tfs/Umbrella]$/Umbrella/Umbrella/Projects;C8566

--- Additions/ImageLoader.cs
+++ Additions/ImageLoader.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -28,20 +29,73 @@ namespace Com.Nostra13.Universalimageloader.Core
 			return LoadImageAsync(ct, uri, imageView, targetSize, null);
 		}
 
-		public async Task<Bitmap> LoadImageAsync(CancellationToken ct, string uri, ImageView imageView, Size? targetSize, DisplayImageOptions options)
+		private static readonly Dictionary<string, Task> CurrentRequestForUri = new Dictionary<string, Task>();
+
+		public async Task<Bitmap> LoadImageAsync(CancellationToken ct, string uri, ImageView imageView, Size? targetSize,
+			DisplayImageOptions options)
+		{
+			try
+			{
+				for(var retryNo=0; retryNo < 12; retryNo++)
+				{
+					Task taskToWait;
+					Task<Bitmap> taskToRun = null;
+
+					lock (CurrentRequestForUri)
+					{
+						// Any other concurrent request to this uri?
+						if (!CurrentRequestForUri.TryGetValue(uri, out taskToWait)) // yes: we assign it to the variable.
+						{
+							// Nope, we're the only one (or first one), we create
+							// a semaphore others will wait on.
+							taskToRun = InnerLoadImageAsync(ct, uri, imageView, targetSize, options);
+							CurrentRequestForUri[uri] = taskToRun;
+						}
+					}
+
+					if (taskToRun != null)
+					{
+						return await taskToRun;
+					}
+					else if (taskToWait != null)
+					{
+						await taskToWait; // wait for other before retrying
+
+						if (retryNo > 1)
+						{
+							// After 1st retry, wait for others to execute before retrying to
+							// minimize competing tasks issues.
+							await Task.Delay(retryNo * 5, ct);
+						}
+					}
+				}
+
+				throw new InvalidOperationException("Retry count exceed.");
+			}
+			finally
+			{
+				lock (CurrentRequestForUri)
+				{
+					// Remove waiting entry in dictionary
+					CurrentRequestForUri.Remove(uri);
+				}
+			}
+		}
+
+		private static async Task<Bitmap> InnerLoadImageAsync(CancellationToken ct, string uri, ImageView imageView, Size? targetSize, DisplayImageOptions options)
 		{
 			TaskCompletionSource<Android.Graphics.Bitmap> source = new TaskCompletionSource<Android.Graphics.Bitmap>();
-			
+
 			options = options ?? new DisplayImageOptions.Builder()
 				.CacheInMemory(true)
 				.CacheOnDisk(true)
 				.Build();
 
 			using (var aware =
-				imageView != null ?
-					new ImageViewAwareCancellable(imageView, ct) :
-					null
-			)
+				imageView != null
+					? new ImageViewAwareCancellable(imageView, ct)
+					: null
+				)
 			{
 				using (var listener = new ImageListener(source))
 				{
@@ -55,7 +109,7 @@ namespace Com.Nostra13.Universalimageloader.Core
 							aware,
 							options,
 							listener
-						);
+							);
 					}
 					else if (targetSize != null && imageView == null)
 					{
@@ -66,7 +120,7 @@ namespace Com.Nostra13.Universalimageloader.Core
 							targetImageSize,
 							options,
 							listener
-						);
+							);
 					}
 					else
 					{
@@ -74,7 +128,7 @@ namespace Com.Nostra13.Universalimageloader.Core
 							uri,
 							options,
 							listener
-						);
+							);
 					}
 
 					var target = await source.Task;
@@ -159,7 +213,7 @@ namespace Com.Nostra13.Universalimageloader.Core
 
 			public void OnLoadingCancelled(string p0, Android.Views.View p1)
 			{
-				_source.TrySetResult(null);
+				_source.SetCanceled();
 			}
 
 			public void OnLoadingComplete(string p0, Android.Views.View p1, Android.Graphics.Bitmap bitmap)
